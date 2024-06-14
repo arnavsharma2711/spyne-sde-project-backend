8e12f214a728314cf4a1439525418a8cc1a3d56c
Arnav Sharma <arnavsharma2711@gmail.com>
2024-06-14 13:16:27 +0530

    Remove Migrations

--- migrations/0008_ordinary_randall.sql
+++
@@ -1,4 +0,0 @@
-DROP INDEX IF EXISTS "follower_mappings_follower_id_idx";--> statement-breakpoint
-DROP INDEX IF EXISTS "follower_mappings_following_id_idx";--> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "follower_mappings_follower_id_idx" ON "follower_mappings" USING btree ("follower_id");--> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "follower_mappings_following_id_idx" ON "follower_mappings" USING btree ("following_id");
